b547c430bb9dc87d2eb6d363b77d561af58d3ea4
Dan Habot <dan.habot@gmail.com>
2020-12-09 18:06:08 -0500

    files can be retrived from virtual disk

--- corrupt.py
+++ corrupt.py
@@ -10,47 +10,83 @@ class Disk:
   def __init__(self, fragsize, fragdev):
     self._fragsize = fragsize
     self._fragdev = fragdev
-    self._manifest = dict()
+    self.manifest = dict()
     self._memory = list()
     self._staging = list()
     self._idcount = 0
 
   def add_file(self, path, bits, filler):
+    c = list(chunks(bits, self._fragsize))
+    c.reverse()
     self._staging.append({
       "id": self._idcount,
       "path": path,
-      "chunks": list(chunks(bits, self._fragsize)),
+      "chunks": c,
       "isFiller": filler
     })
     self._idcount += 1
+    
+  def _getStagedChunks(self):
+    return sum([len(x["chunks"]) for x in self._staging])
 
   def flush(self):
+    initialChunkCount = float(self._getStagedChunks())
     while all([len(x["chunks"]) > 0 for x in self._staging]):
       file = choice(self._staging)
       start = len(self._memory)
 
-      for _ in range(randint(1, self._fragdev)):
+      for _ in range(min(len(file["chunks"]), randint(1, self._fragdev))):
         self._memory.extend(file["chunks"].pop())
 
       end = len(self._memory)
 
-      if file["id"] not in self._manifest.keys():
-        self._manifest[file["id"]] = list()
+      if file["id"] not in self.manifest.keys():
+        self.manifest[file["id"]] = {
+          "chunks": list(),
+          "deleted": False,
+          "path": file["path"]
+        }
 
-      self._manifest[file["id"]].append((start, end))
-      
+      self.manifest[file["id"]]["chunks"].append((start, end))
+      yield (initialChunkCount - self._getStagedChunks()) / initialChunkCount
+
+    self._staging = list()
+
+  def delete_file(self, path):
+    for id, entry in self.manifest.items():
+      entry["deleted"] = entry["path"] == path
+
+  def reformat(self, fragsize, fragdev):
+    self._fragsize = fragsize
+    self._fragdev = fragdev
+
+  def get_file(self, path):
+    fileentry = None
+    for id, entry in self.manifest.items():
+      if entry["path"] == path:
+        fileentry = entry
+        break
+
+    buffer = bytearray()
+    for start, end in fileentry["chunks"]:
+      buffer.extend(self._memory[start:end])
+
+    return buffer
+
 
 def main():
   print("Starting...")
-  disk = Disk(1024, 4)
+  disk = Disk(2**10, 8)
+
   for file in os.listdir('good_pics'):
     with open('good_pics/' + file, mode='rb') as f:
       print("Adding file " + file)
-      disk.add_file(file, f.read(), False)
+      disk.add_file(file, bytearray(f.read()), False)
 
   print("Added all files, flushing")
-  disk.flush()
-  print("Flushing done")
+  for progress in disk.flush():
+    print("Flush progress {:.2f}%".format(progress), end='\r')
+  print("\nFlushing done")
 
 
 if __name__ == "__main__":
