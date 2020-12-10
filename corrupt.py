c1f3360f20f8c672cc2613455857a80c491c3904
Dan Habot <dan.habot@gmail.com>
2020-12-10 00:34:16 -0500

    commit

--- corrupt.py
+++ corrupt.py
@@ -4,7 +4,7 @@ import os
 import sys
 import json
 
-REPEAT_FILES = 20
+REPEAT_FILES = 10
 
 class Disk:
   def __init__(self, fragsize, fragdev):
@@ -14,6 +14,8 @@ class Disk:
     self.memory = list()
     self._staging = list()
     self._idcount = 0
+    self._fresh = True
+    self._freshnessPoint = 0
 
   def add_file(self, path, filler):
     filesize = os.path.getsize(path)
@@ -41,11 +43,34 @@ class Disk:
       file = choice(self._staging)
 
       if len(file["chunks"]) > 0:
-        for _ in range(min(len(file["chunks"]), randint(1, self._fragdev))):
-          self.memory.append({
-            "id": file["id"],
-            "chunk": file["chunks"].pop()
-          })
+        if not self._fresh:
+          i = self._freshnessPoint
+          fdev = randint(1, self._fragdev)
+          deletedFiles = list(filter(lambda x : x["deleted"], self.manifest))
+          deletedFileIDs = list(map(lambda x : x["id"], deletedFiles))
+
+          while i < len(self.memory) and self.memory[i]["id"] not in deletedFileIDs:
+            i += 1
+
+          if i >= len(self.memory):
+            self._fresh = True
+            continue
+
+          while fdev >= 0 and self.memory[i]["id"] in deletedFileIDs and len(file["chunks"]) > 0:
+            self.memory[i] = {
+              "id": file["id"],
+              "chunk": file["chunks"].pop()
+            }
+            fdev -= 1
+            i += 1
+
+          self._freshnessPoint = i
+        else:
+          for _ in range(min(len(file["chunks"]), randint(1, self._fragdev))):
+            self.memory.append({
+              "id": file["id"],
+              "chunk": file["chunks"].pop()
+            })
       else:
         self.manifest.append({
           "id": file["id"],
@@ -58,42 +83,48 @@ class Disk:
       yield (initialChunkCount - self._getStagedChunks()) / initialChunkCount
 
     self._staging = list()
+    self._fresh = False
 
-  def delete_file(self, path):
-    for id, entry in self.manifest.items():
-      entry["deleted"] = entry["path"] == path
+  def deleteFile(self, id):
+    for item in self.manifest:
+      if (item["id"] == id):
+        item["deleted"] = True
+        return
 
-  def reformat(self, fragsize, fragdev):
-    self._fragsize = fragsize
-    self._fragdev = fragdev
+  def getFile(self, path):
 
-  def get_file(self, path):
-    fileentry = None
-    for id, entry in self.manifest.items():
-      if entry["path"] == path:
-        fileentry = entry
-        break
-
-    buffer = bytearray()
-    for start, end in fileentry["chunks"]:
-      buffer.extend(self._memory[start:end])
-
-    return buffer
 
 
 def main():
   print("Starting...")
-  disk = Disk(2**10, 8)
+  disk = Disk(2**11, 8)
 
-  for file in os.listdir('good_pics'):
-    with open('good_pics/' + file, mode='rb') as f:
+  def AddFiles(dir, repeat, filler):
+    for file in os.listdir(dir):
       print("Adding file " + file)
-      disk.add_file(file, bytearray(f.read()), False)
 
-  print("Added all files, flushing")
-  for progress in disk.flush():
-    print("Flush progress {:.2f}%".format(progress), end='\r')
-  print("\nFlushing done")
+      for i in range(repeat):
+        disk.add_file(dir + '/' + file, filler)
+        print("Progress {:.2f}%".format(i / REPEAT_FILES), end='\r')
+
+  AddFiles('good_pics', 3, False)
+  AddFiles('filler_data', 7, True)
+
+  def Flush():
+    print("Flushing")
+    for progress in disk.flush():
+      print("Flush progress {:.3f}%".format(progress), end='\r')
+    print("\nFlushing done")
+
+  Flush()
+
+  print("Deleting files at random")
+  for deleteFile in sample(disk.manifest, int(len(os.listdir('good_pics')) + len(os.listdir('filler_data')) * 0.7) ):
+    disk.deleteFile(deleteFile["id"])
+  print("Done")
+  
+  AddFiles('filler_data', 10, True)
+  Flush()
 
 
 if __name__ == "__main__":
